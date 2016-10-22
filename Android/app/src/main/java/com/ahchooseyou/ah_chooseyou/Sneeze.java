a017b9bf6962c1f5143458c093dc6d012fabdf03
LilyEssence <lily.essence.two@gmail.com>
2016-10-22 16:57:56 -0500

    Sneeze class

---
+++ Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/Sneeze.java
@@ -0,0 +1,17 @@
+package com.ahchooseyou.ah_chooseyou;
+
+import java.util.Date;
+
+/**
+ * Created by Lily on 10/22/2016.
+ */
+
+public class Sneeze {
+    String username;
+    Date date;
+
+    public Sneeze(String username, Date date){
+        this.username = username;
+        this.date = date;
+    }
+}

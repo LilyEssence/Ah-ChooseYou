ea2966bb1a281d556e473499c658f82d77b9d19a
Saad Najmi <saadnajmi2@gmail.com>
2016-10-22 13:50:04 -0500

    Added a Find Match Activity

---
+++ Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/FindMatchActivity.java
@@ -0,0 +1,13 @@
+package com.ahchooseyou.ah_chooseyou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FindMatchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_match);
+    }
+}
--- Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/MainActivity.java
+++ Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -15,5 +17,10 @@ public class MainActivity extends Activity {
         if (!LoginActivity.isLoggedIn) {
             startActivity(new Intent(this, LoginActivity.class));
         }
+    }
+
+    public void sneeze(View view) {
+        Toast.makeText(this, R.string.you_sneezed, Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(this, FindMatchActivity.class));
     }
 }

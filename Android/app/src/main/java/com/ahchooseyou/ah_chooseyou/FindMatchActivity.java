1cd750ad6f3c4a5d60876c921f8a4fb448f0d1da
Saad Najmi <saadnajmi2@gmail.com>
2016-10-22 17:19:58 -0500

    Facebook Login Button

--- Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/FindMatchActivity.java
+++ Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/FindMatchActivity.java
@@ -2,6 +2,8 @@ package com.ahchooseyou.ah_chooseyou;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class FindMatchActivity extends Activity {
 
@@ -9,5 +11,9 @@ public class FindMatchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_match);
+    }
+
+    public void messageYourMatch(View view) {
+        Toast.makeText(this, R.string.not_yet_implemented, Toast.LENGTH_SHORT).show();
     }
 }
--- Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/LoginActivity.java
+++ Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/LoginActivity.java
@@ -5,16 +5,60 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
 public class LoginActivity extends Activity {
 
+    CallbackManager callbackManager;
     public static boolean isLoggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
         setContentView(R.layout.activity_login);
         isLoggedIn = true;
 
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                loginResult.getAccessToken();
+
+                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                loginIntent.putExtra("isLoggedIn", true);
+                startActivity(loginIntent);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void login(View view) {

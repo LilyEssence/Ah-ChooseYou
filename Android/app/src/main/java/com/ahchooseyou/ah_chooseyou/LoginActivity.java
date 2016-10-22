894c7083c009426925a4b8d8ea67ba52eda52e42
Saad Najmi <saadnajmi2@gmail.com>
2016-10-22 18:37:14 -0500

    Added logout button and a Name Text View

--- Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/LoginActivity.java
+++ Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/LoginActivity.java
@@ -2,7 +2,12 @@ package com.ahchooseyou.ah_chooseyou;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 
 import com.facebook.CallbackManager;
@@ -12,11 +17,13 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 public class LoginActivity extends Activity {
 
     CallbackManager callbackManager;
-    public static boolean isLoggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,6 @@ public class LoginActivity extends Activity {
         callbackManager = CallbackManager.Factory.create();
 
         setContentView(R.layout.activity_login);
-        isLoggedIn = true;
 
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
@@ -36,10 +42,7 @@ public class LoginActivity extends Activity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 // App code
-                loginResult.getAccessToken();
-
                 Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
-                loginIntent.putExtra("isLoggedIn", true);
                 startActivity(loginIntent);
             }
 
@@ -53,17 +56,26 @@ public class LoginActivity extends Activity {
                 // App code
             }
         });
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.ahchooseyou.ah_chooseyou",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
-    }
-
-    public void login(View view) {
-        Intent loginIntent = new Intent(this, MainActivity.class);
-        loginIntent.putExtra("isLoggedIn", true);
-        startActivity(loginIntent);
     }
 }
--- Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/MainActivity.java
+++ Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/MainActivity.java
@@ -5,22 +5,54 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
-        if (!LoginActivity.isLoggedIn) {
+
+        if (!isLoggedIn()) {
+            Toast.makeText(this, "Nobody logged in", Toast.LENGTH_SHORT).show();
             startActivity(new Intent(this, LoginActivity.class));
         }
+
+        TextView tv = (TextView) findViewById(R.id.textView2);
+        tv.setText(Profile.getCurrentProfile().getName());
+
+
+
     }
 
     public void sneeze(View view) {
         Toast.makeText(this, R.string.you_sneezed, Toast.LENGTH_SHORT).show();
         startActivity(new Intent(this, FindMatchActivity.class));
+    }
+
+    public boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
+    }
+
+    public void logout(View view) {
+        LoginManager.getInstance().logOut();
+        startActivity(new Intent(this, LoginActivity.class));
+
     }
 }

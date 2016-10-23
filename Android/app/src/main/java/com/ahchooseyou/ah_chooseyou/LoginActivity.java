bf9de16f3d96f4e2a51f1fca91d34f921b6557f4
Saad Najmi <saadnajmi2@gmail.com>
2016-10-23 00:14:02 -0500

    programmatically get fb hash

--- Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/LoginActivity.java
+++ Android/app/src/main/java/com/ahchooseyou/ah_chooseyou/LoginActivity.java
@@ -37,6 +37,23 @@ public class LoginActivity extends Activity {
 
         setContentView(R.layout.activity_login);
 
+        //get hash
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.packagename",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
+
         LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
 
